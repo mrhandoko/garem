b6e45d54f60f9fd52fee27a93e35956093365ae4
Bambang Handoko <bambank.handoko@yahoo.com>
2017-03-26 16:27:19 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var garem = require('./index.js')
 
 console.log(garem(6))
-// Prints Number Character, for example: 9bzL21, dCCnny, etc
+// Prints Number Character with length 6, for example: 9bzL21, dCCnny, etc
 
 // Or you can add it as salt on crypto Node js
 const crypto = require('crypto');
